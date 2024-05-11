11195bb1bf6adcff7bae89ae1041af5f98c5b19c
karishita <karishita025@gmail.com>
2024-05-11 18:16:29 +0530

    My Commit message

--- backend/src/main/java/com/thinkxfactor/attendNow/AttendNowApplication.java
+++ backend/src/main/java/com/thinkxfactor/attendNow/AttendNowApplication.java
@@ -2,12 +2,29 @@ package com.thinkxfactor.attendNow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AttendNowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AttendNowApplication.class, args);
+
 	}
 
+}
+//hello world API
+@RestController
+@RequestMapping("/api")
+class TestController{
+
+	@GetMapping("/hello")
+	public String hello()
+	{
+		System.out.println("Welcome");
+		return "Hello World from controller";
+
+	}
 }
